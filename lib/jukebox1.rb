471ff34ff34adc996b8e2f1256105672eeee31f7
smithwebtek <brad@smithwebtek.com>
2018-08-15 15:26:02 -0400

    Done.

---
+++ lib/jukebox1.rb
@@ -0,0 +1,38 @@
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+
+def say_hello(name)
+	puts "Hi, #{name}!"
+end
+
+def help
+	commands = [
+		'help',
+		'list',
+		'play',
+		'exit'
+	]
+	commands.each do |command|
+		puts command
+	end
+end
+
+def play
+
+	binding.pry 
+	
+	puts "Please enter a song name or number"
+	inpu
+
+
+end
